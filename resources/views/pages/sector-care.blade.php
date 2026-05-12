769ca6ea0379e4f55ff752899bc3d2b94f3411ce
João Gabriel Pereira Guimarães <147404902+JoaoGuimaraesCCT@users.noreply.github.com>
2026-05-12 07:39:30 +0100

    Shift Parts and Aftercare card image to right position

--- resources/views/pages/sector-care.blade.php
+++ resources/views/pages/sector-care.blade.php
@@ -263,7 +263,7 @@
                     'cta'    => 'Explore Support Options',
                     'route'  => route('service-contracts'),
                     'img'    => '/images/healthcare/services-overview-hero.jpg',
-                    'pos'    => 'left 40%',
+                    'pos'    => 'right 40%',
                 ],
             ] as $card)
             <a href="{{ $card['route'] }}"
